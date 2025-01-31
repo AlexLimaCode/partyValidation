976b97240995367b8d4ddc8777a0773e5be00498
SolucionesTecnologicasZtech <157165804+SolucionesTecnologicasZtech@users.noreply.github.com>
2025-01-31 15:44:51 -0600

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,10 @@ function App() {
 
   return (
     <div className="background">
-      {invitationId === "" ? (
+      {console.log(invitationId)}
+      {invitationId === "" ||
+      invitationId === null ||
+      invitationId === undefined ? (
         <div className="error-container">
           <h1 className="error-title">Ops..</h1>
           <p className="error-message">
@@ -94,7 +97,7 @@ function App() {
                   </label>
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3766.373502827694!2d-99.66419192447394!3d19.266117045975975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd89a0b674a73f%3A0x329d596bc77c7d7e!2sAnthuriums%20Hall%20and%20Garden!5e0!3m2!1sen!2smx!4v1738350986632!5m2!1sen!2smx"
-                    width="100%"
+                    width="300"
                     height="350"
                     style={{ border: "0" }}
                     allowFullScreen={true}
